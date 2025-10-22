5e6f0d3f4311e6fc35308aa228ed425a3aefce36
Egor Egerev <maltoleb@gmail.com>
2025-10-22 07:01:26 +0300

    step3 project2, added file1,2.json and updated main.py with json code

--- gendiff/main.py
+++ gendiff/main.py
@@ -1,9 +1,13 @@
 from gendiff import cli
+import json
 
 
 def main():
     args = cli.parse_args()
-    print(args)
+    data1 = json.load(open(args.first_file))
+    data2 = json.load(open(args.second_file))
+    print(data1)
+    print(data2)
 
 
 if __name__ == '__main__':

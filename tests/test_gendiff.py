ab4c983469833c63b11224e9fae7640808fe7ea3
Egor <maltoleb@gmail.com>
2025-10-25 21:40:37 +0300

    added tests step 5

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -1,2 +1,15 @@
-def test_dummy():
-    assert True
+from pathlib import Path
+from gendiff.generate_diff import generate_diff
+
+
+def test_flat_json():
+    base = Path('tests/test_data')
+    file1 = base / 'file1.json'
+    file2 = base / 'file2.json'
+    expected = base / 'expected_flat.txt'
+    result = generate_diff(file1, file2)
+    
+    with open(expected) as f:
+        expected_content = f.read()
+        
+    assert result.strip() == expected_content.strip()

49e794f4136f3c9d8d6a35222d6c129c6c80b256
Egor Egerev <maltoleb@gmail.com>
2025-11-05 10:52:07 +0300

    fix: pass format argument from CLI to generate_diff

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -4,7 +4,7 @@ from gendiff.generate_diff import generate_diff
 
 def main():
     args = cli.parse_args()
-    diff = generate_diff(args.first_file, args.second_file)
+    diff = generate_diff(args.first_file, args.second_file, args.format)
     print(diff)
 
 
